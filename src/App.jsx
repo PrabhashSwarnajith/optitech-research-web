124710c0ebd3b612797a469fb129b48928a8db48
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-05-25 21:10:44 +0530

    feat: implement main application layout with routing components

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Home from './pages/Home';
+import Domain from './pages/Domain';
+import Navbar from './components/Navbar';
+import Contact from './pages/Contact';
+import Milestones from './pages/Milestones';
+import Documents from './pages/Documents';
+import Presentations from './pages/Presentations';
+
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+     <Navbar />
+      <main className="flex-grow">
+        <Home />
+        <Domain />
+        <Milestones />
+        <Documents />
+        <Presentations />
+        <Contact />  
+       
+      </main>
+      
+    </div>
+    
+
+  );
+}
+
+export default App;
